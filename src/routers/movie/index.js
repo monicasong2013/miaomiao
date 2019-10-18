7ee6e34d2e26fcd4f333f34792166d83ac936af9
monicasong2013 <1793802621@qq.com>
2019-10-18 14:43:01 +0800

    add createComponents

---
+++ src/routers/movie/index.js
@@ -0,0 +1,26 @@
+export default{
+    path:'/movie',
+    component:()=>import('@/views/Movie'),
+    children:[
+        {
+            path:'City',
+            component:()=>import('@/components/City')
+        },
+        {
+            path:'NowPlaying',
+            component:()=>import('@/components/NowPlaying')
+        },
+        {
+            path:'ComingSoon',
+            component:()=>import('@/components/ComingSoon')
+        },
+        {
+            path:'Search',
+            component:()=>import('@/components/Search')
+        },
+        {
+            path:'/movie',
+            redirect:'/movie/nowPlaying'
+        }
+    ]
+}
